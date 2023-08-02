8a481f21becb11f2be8aeba1e5c3a63835d77ec4
German Blejman <gblejman@gmail.com>
2023-08-02 15:40:47 -0300

    add nav to home when clicking logo

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,7 +5,9 @@ import { Status } from './Status';
 export const Nav = () => {
   return (
     <nav className="flex  bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700 p-5">
-      <Image src="/bitfinex-logo.svg" alt="Bitfinex Logo" width={180} height={37} priority />
+      <Link href="/">
+        <Image src="/bitfinex-logo.svg" alt="Bitfinex Logo" width={180} height={37} priority />
+      </Link>
       <div className="flex flex-1 pl-5">
         <Link href="/ws-messages">Websocket Msgs</Link>
       </div>
