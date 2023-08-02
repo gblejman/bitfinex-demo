061a0d65ed6bb819f22defa9542e2b2982c35b40
German Blejman <gblejman@gmail.com>
2023-08-02 15:34:03 -0300

    add nav link to websocket msgs

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { Status } from './Status';
 
 export const Nav = () => {
   return (
-    <nav className="flex justify-between bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700 p-5">
+    <nav className="flex  bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700 p-5">
       <Image src="/bitfinex-logo.svg" alt="Bitfinex Logo" width={180} height={37} priority />
+      <div className="flex flex-1 pl-5">
+        <Link href="/ws-messages">Websocket Msgs</Link>
+      </div>
       <Status />
     </nav>
   );
